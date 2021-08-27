1d952f949f627918c648c75a87e558da64d87426
emilywilbur <ewilbur94@gmail.com>
2021-08-27 10:11:14 -0400

    finishing up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,27 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
 
-import { AppComponent } from './app.component';
-import { ProductListComponent } from './products/product-list.component';
+import { HttpClientModule } from "@angular/common/http";
+
+import { AppComponent } from "./app.component";
+
+import { WelcomeComponent } from "./home/welcome.component";
+import { RouterModule } from "@angular/router";
+
+import { ProductModule } from "./products/product.module";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ProductListComponent
-  ],
+  declarations: [AppComponent, WelcomeComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    RouterModule.forRoot([
+      { path: "welcome", component: WelcomeComponent },
+      { path: "", redirectTo: "welcome", pathMatch: "full" },
+      { path: "**", redirectTo: "welcome", pathMatch: "full" },
+    ]),
+    ProductModule,
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
