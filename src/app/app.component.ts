fa88ad8d894a9989aed798f8026a01a2b00d6fd8
emilywilbur <ewilbur94@gmail.com>
2021-08-27 12:08:04 -0400

    try again

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,11 @@ import { Component } from "@angular/core";
 @Component({
   selector: "pm-root",
   template: `
-  <nav class='navbar navbar-expand navbar-light bg-light'>
-  <a class='navbar-brand'>{{pageTitle}}</a>
+  <nav class='navbar navbar-expand navbar-light bg-dark'>
+  <a class='navbar-brand' style="padding:10px; color:white">{{pageTitle}}</a>
   <ul class='nav nav-pills'>
-    <li><a class='nav-link' routerLink='/welcome'>Home</a></li>
-    <li><a class='nav-link' routerLink='/products'>Product List</a></li>
+    <li><a class='nav-link'  routerLink='/welcome' style="color:white">Home</a></li>
+    <li><a class='nav-link' routerLink='/products' style="color:white">Product List</a></li>
   </ul>
   </nav>
   <div class='container'>
